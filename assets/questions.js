3dbc06e67a208fab8cf0dcb87e7fe3f11e5f32bf
John <johnhamborsky94@gmail.com>
2022-03-27 14:58:19 -0400

    updated css, js, html

---
+++ assets/questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, opitons, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "String values must be enclosed with ________ when being assigned to variables",
+        answer: "curly brackets",
+        options: [
+            "nothing",
+            "curly brackets",
+            "quotes",
+            "parentheses"
+        ]
+    },
+    {
+        numb: 2,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "console.log",
+        options: [
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store ________",
+        answer: "all of the above",
+        options: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Cascading Style Sheet",
+            "Colorful Style Sheet",
+            "Changing Style Sheet",
+            "Computer Style Sheet"
+        ]
+    },
+    {
+        numb: 5,
+        question: "_______ tools can be used to add possible changes to a webpage without actually changing it's content",
+        answer: "Developer",
+        options: [
+            "Change",
+            "Computer",
+            "Task changer",
+            "Developer"
+        ]
+    },
+];
